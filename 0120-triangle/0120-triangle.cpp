cf8247f34c59851ca948637c5d3f45eec7636f6f
helljez <jesvinjaisonkurisingal@gmail.com>
2025-08-13 18:45:25 +0530

    Time: 0 ms (100%), Space: 13.4 MB (11.52%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,28 +1,30 @@
 class Solution {
+
 private:
-    int rec(vector<vector<int>> &dp, vector<vector<int>> &triangle, int x, int i) {
-        if (x < 0)
+
+    int rec(vector<vector<int>> &dp,vector<vector<int>> &triangle,int x,int i){
+        if(x<0)
             return 0;
-        if (i < 0 || i > x)
-            return INT_MAX; // invalid index in triangle
+        if(i<0||i>x)
+            return INT_MAX;
 
-        if (dp[x][i] != -1)
+        if(dp[x][i]!=-1){
             return dp[x][i];
+        }
+        return dp[x][i]=triangle[x][i]+min(rec(dp,triangle,x-1,i-1),rec(dp,triangle,x-1,i));
 
-        return dp[x][i] = triangle[x][i] +
-                          min(rec(dp, triangle, x - 1, i - 1),
-                              rec(dp, triangle, x - 1, i));
     }
-
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        int globmin = INT_MAX; // find minimum, not maximum
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        
+        int n=triangle.size();
+        int globmax=INT_MAX;
+        vector<vector<int>> dp(n,vector<int> (n,-1));
 
-        for (int i = 0; i < n; i++) {
-            globmin = min(globmin, rec(dp, triangle, n - 1, i));
+        for(int i=0;i<n;i++){
+            globmax=min(globmax,rec(dp,triangle,n-1,i));
         }
-        return globmin;
+        return globmax;
+
     }
 };
