bc2bd85d43c58bfa9d2572557e56dc598a5ddd36
helljez <jesvinjaisonkurisingal@gmail.com>
2025-08-15 14:41:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.41%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        
+        if(n<=0)
+            return false;
+
+        return !(n&n-1)&&(n&0x55555555);
+        
+
+        
+    }
+};
