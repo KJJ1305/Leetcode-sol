378d85d300113f3ca8683443ef729415426eb2ea
helljez <jesvinjaisonkurisingal@gmail.com>
2025-08-20 19:21:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (37.06%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+private:
+    int fibo(int n,vector<int> &dp){
+        if(n<=2) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+    }
+
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return fibo(n,dp);
+    }
+};
